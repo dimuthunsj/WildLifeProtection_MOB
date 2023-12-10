7ed6abdc57625027ae5dc417edd561a7e51c4313
SL/dis <dimuthu_sampath@trimble.com>
2023-12-10 21:57:52 +0530

    inital commit for mobile version

---
+++ src/main/java/com/wildprotection/wildlifeprotection_mob/WildLifeProtectionMobApplication.java
@@ -0,0 +1,13 @@
+package com.wildprotection.wildlifeprotection_mob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildLifeProtectionMobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildLifeProtectionMobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildprotection/wildlifeprotection_mob/WildLifeProtectionMobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildprotection.wildlifeprotection_mob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildLifeProtectionMobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
